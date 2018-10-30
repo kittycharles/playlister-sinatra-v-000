9ee9c3c1559dca30b09e7a3f078dfdd987be3a5b
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-30 21:12:51 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ require 'rack-flash'
 
 class SongsController < ApplicationController
  use Rack::Flash
- 
+
   get '/songs' do
     @songs = Song.all
 
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
-
+binding.pry
     flash[:message] = "Successfully created song."
 
     redirect "/songs/#{@song.slug.downcase}"
