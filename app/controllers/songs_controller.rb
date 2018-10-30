802da76ec69003342843444d199fd730ac3340e5
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-30 20:36:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,11 @@
 require 'pry'
 class SongsController < ApplicationController
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
   enable :sessions
   use Rack::Flash
-  
+
   get '/songs' do
     @songs = Song.all
 
