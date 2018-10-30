07c3ba6012730dca1c87559edf937fc81636da0c
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-30 21:07:58 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,5 +3,5 @@ class Artist < ActiveRecord::Base
     extend Slugifiable::ClassMethods
   has_many :songs
   has_many :genres, :through=> :songs
-  
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,13 +1,9 @@
 class Genre < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+    extend Slugifiable::ClassMethods
   has_many :song_genres
   has_many :songs, :through => :song_genres
   has_many :artists, :through => :songs
 
-  def slug
-    self.name.downcase.gsub(" ", "-")
-  end
 
-  def self.find_by_slug(slug)
-    self.all.find {|artist| artist.slug == slug}
-  end
 end
