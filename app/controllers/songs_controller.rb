d9e831acfd9625411703aa95d9c9749949f0fab4
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-30 21:39:42 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,7 +23,7 @@ class SongsController < ApplicationController
     @song = Song.create(name: params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
-    @song.save]
+    @song.save
 
     flash[:message] = "Successfully created song."
 
